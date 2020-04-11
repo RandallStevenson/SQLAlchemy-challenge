127a34292c4c048e4c26e125f80a822edf42e9fe
RandallStevenson <59617625+RandallStevenson@users.noreply.github.com>
2020-04-11 10:35:59 -0500

    closed sessions

--- app.py
+++ app.py
@@ -29,7 +29,13 @@ def home():
 
 @app.route("/api/v1.0/precipitation")
 def precip():
-    
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base=automap_base()
+    Base.prepare(engine,reflect=True)
+    Station=Base.classes.station
+    Measurement=Base.classes.measurement
+    session=Session(bind=engine)
+    max_stn='USC00519281'
     sel=[Measurement.date,Measurement.prcp]
     prcp_query=session.query(*sel).\
         filter(Measurement.station==max_stn).\
@@ -38,18 +44,34 @@ def precip():
     prcp_df.set_index(prcp_df['date'],inplace=True)
     prcp_df=prcp_df.drop(['date'],axis=1)
     prcp_dict=prcp_df.to_dict()['prcp']
+    session.close()
     return jsonify(prcp_dict)
 
 @app.route("/api/v1.0/stations")
 def statns():
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base=automap_base()
+    Base.prepare(engine,reflect=True)
+    Station=Base.classes.station
+    Measurement=Base.classes.measurement
+    session=Session(bind=engine)
+    max_stn='USC00519281'
     fr_stn=session.query(Station.station,Station.name)
     stn_list=pd.DataFrame(fr_stn,columns=["station","name"])
     stn_list.set_index(stn_list["station"],inplace=True)
     stn_list=stn_list.drop(["station"],axis=1)
+    session.close()
     return jsonify(stn_list.to_dict()['name'])
 
 @app.route("/api/v1.0/tobs")
 def temps():
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base=automap_base()
+    Base.prepare(engine,reflect=True)
+    Station=Base.classes.station
+    Measurement=Base.classes.measurement
+    session=Session(bind=engine)
+    max_stn='USC00519281'
     sel=[Measurement.date,Measurement.tobs]
     daily_temps=session.query(*sel).\
         filter(Measurement.station==max_stn).\
@@ -58,10 +80,18 @@ def temps():
     temp_df.set_index(temp_df['date'],inplace=True)
     temp_df=temp_df.drop(['date'],axis=1)
     temp_dict=temp_df.to_dict()['tobs']
+    session.close()
     return jsonify(temp_dict)
 
 @app.route("/api/v1.0/<start>")
 def tempfrom(start):
+    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+    Base=automap_base()
+    Base.prepare(engine,reflect=True)
+    Station=Base.classes.station
+    Measurement=Base.classes.measurement
+    session=Session(bind=engine)
+    max_stn='USC00519281'
     sd_yr=int(start[0:4])
     sd_mo=int(start[5:7])
     sd_dy=int(start[8:10])
@@ -71,6 +101,7 @@ def tempfrom(start):
         filter(Measurement.station==max_stn).\
         filter(Measurement.date>start_date).all()
     tempfrom_dict={'min_temp':tempfrom_query[0][0],'avg_temp':round(tempfrom_query[0][1],1),'max_temp':tempfrom_query[0][2]}
+    session.close()
     return jsonify(tempfrom_dict)
 
 @app.route("/api/v1.0/<start>/<end>")
@@ -89,6 +120,7 @@ def tempbetween(start,end):
         filter(Measurement.date>start_date).\
         filter(Measurement.date<end_date).all()
     tempin_dict={'min_temp':tempin_query[0][0],'avg_temp':round(tempin_query[0][1],1),'max_temp':tempin_query[0][2]}
+    session.close()
     return jsonify(tempin_dict)
 
 if __name__=="__main__":
